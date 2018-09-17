2e62cba2f95f9c49173b1f200717b6040b385bb6
andy reyes <11710reyes@gmail.com>
2018-09-17 21:21:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function shout(string){return string.toUpperCase()}
 function whisper(string){return string.toLowerCase()}
 function logShout(string){console.log("HELLO")}
+<<<<<<< HEAD
 function logWhisper(string){console.log('hello')}
 function sayHiToGrandma(string) {if (string===string.toUpperCase()) {return "YES INDEED!"} 
 else if (string===string.toLowerCase()){return "I can't hear you!"} 
 else if (string==="I love you, Grandma."){return "I love you, too."}}
+=======
+>>>>>>> 5e0c754a8d0840973fb12a3d9cb45b73f0e0a38b
